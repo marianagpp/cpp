4500cc12287c36a0665ba30b7b166436b05d9dcd
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-09-06 10:52:52 -0500

    Create S3 - E6.1 - do while.cpp

---
+++ S3 - E6.1 - do while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x;
+    cin >> x;
+    char c;
+
+    cin >> c;
+    do{
+        cout << c <<"\n";
+        }
+    while (x--);
+
+    return 0;}
