c7595b99da91a3229d3a7c3179602ed28603bcca
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-09-06 10:49:04 -0500

    Create S3 - E5 - while.cpp

---
+++ S3 - E5 - while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x;
+    cin >> x;
+    char c;
+
+    cin >> c;
+    int i=0;
+    while (i<x){
+        cout << c <<"\n";
+        i++;}
+
+    return 0;}
+
