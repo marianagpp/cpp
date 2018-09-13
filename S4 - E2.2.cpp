26d1f86f1d29a107ac76454d16fda71f28d7976f
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-09-13 10:37:41 -0500

    Create S4 - E2.2.cpp

---
+++ S4 - E2.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int x;
+    cout << "Ingresa un número: ";
+    cin >> x;
+    double i=0,p=0;
+    int c=1;
+
+while (c<x){
+    p=(pow(2,c));
+    c++;
+    if (p<=x){
+        cout << p;}
+}
+    return 0;
+}
+
+\\solo el ultimo numero del print es la respuesta
