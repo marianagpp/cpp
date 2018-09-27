327ad802c06c321099d0e72ab02866b078acfa7a
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-09-27 10:21:55 -0500

    Create S6 - array tamaño N #rand.cpp

---
+++ S6 - array tamaño N #rand.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+void arrInt(int arrInt[], int n);
+int main()
+{
+    int n;
+    cout << "Ingrese el numero maximo: ";
+    cin >> n;
+    int arrInt[n];
+    arrInt(arrInt,n);
+    return 0;
+}
+
+void arrIntA(int arrInt[], int n);
+    srand(time(nullptr));
+    
+...................
