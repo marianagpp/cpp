4b347ae048e76289ecddf766778a803ac6c68f86
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-10-12 01:07:34 -0500

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -1,32 +1,83 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
 
-    int n;
-    cout<<"Ingrese numero: ";
+    int n, m=0, c, v, h;
+    cout<<"Digite el tamanio de la matriz: ";
     cin>>n;
-    int arrInt[n][n];
-    int m;
-    int p=rand()%4; //movimiento n,s,e,o
+    m=n;
+    int a[n][m];
+    srand(time(nullptr));
+    if (n%2==0){n += 1; m = n;}
+    c = (n/2)+1;
 
-    if (n%2==0){
-        m = n/2;}
-    else {
-        m = (n/2)+1;}
-
+    // matriz inicial
+    cout<<"Matriz inicial:"<<endl;
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
-            arrInt[i][j]=0;
-            cout<<setw(5)<<arrInt[i][j]<<" ";}
+            a[i][j]=0;
+            cout<<setw(5)<<a[i][j]<<" ";}
         cout << "\n";}
+    cout << "\n";
 
-    cout<<"Posicion inicial en el centro de la matriz:"<<"\n";
-    for (int i=0; i<m; i++){
-        for (int j=0; j<m; j++){
-            arrInt[m][m]=1;
-            cout<<setw(5)<<arrInt[m][m]<<" ";}
-        cout << "\n";}
+    // matriz posicion centro
+    cout<<"Matriz posicion centro:"<<endl;
+    for(int i=0;i<n;i++){
+        h=n-c;
+        v=m-c;
+        a[h][v]=1;
+        for(int j=0;j<m;j++){
+            cout<< setw(5) << a[i][j] <<" ";}
+        cout<<"\n";}
+    cout << "\n";
+
+    // movimientos
+    for (int k=0; h<n-2; k++)
+        for (int w=0; v<m-2; w++){
+        int r=rand()%4;
+        cout << "Matriz movimiento "<<w+1<<":"<< endl;
+        switch (r) {
+            case 0: //NORTE
+                for (int i = 0; i < n; i++) {
+                    a[h+1][v] = 1;
+                    for (int j = 0; j < m; j++) {
+                        cout << setw(5) << a[i][j] << " ";}
+                    cout << "\n";}
+                h=h+1;
+                cout << "\n";
+                break;
+
+            case 1: //SUR
+                for (int i = 0; i < n; i++) {
+                    a[h-1][v] = 1;
+                    for (int j = 0; j < m; j++) {
+                        cout << setw(5) << a[i][j] << " ";}
+                    cout << "\n";}
+                h=h-1;
+                cout << "\n";
+                break;
+
+            case 2: //ESTE
+                for (int i = 0; i < n; i++) {
+                    a[h][v+1] = 1;
+                    for (int j = 0; j < m; j++) {
+                        cout << setw(5) << a[i][j] << " ";}
+                    cout << "\n";}
+                v=v+1;
+                cout << "\n";
+                break;
+
+            case 3: //OESTE
+                for (int i = 0; i < n; i++) {
+                    a[h][v-1] = 1;
+                    for (int j = 0; j < m; j++) {
+                        cout << setw(5) << a[i][j] << " ";}
+                    cout << "\n";}
+                v=v-1;
+                cout << "\n";
+                break;
+        }}
 
     return 0;}
