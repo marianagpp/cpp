3ebf5fe17fb62cd5af4e3024b7e3d6ad014c37e6
marianagpp <42775978+marianagpp@users.noreply.github.com>
2018-10-12 17:23:55 -0500

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(){
 
-    int n, m=0, c, v, h;
+    int n, m, c, v, h;
     cout<<"Digite el tamanio de la matriz: ";
     cin>>n;
+    if (n%2==0){n += 1;}
     m=n;
     int a[n][m];
     srand(time(nullptr));
-    if (n%2==0){n += 1; m = n;}
     c = (n/2)+1;
 
     // matriz inicial
@@ -31,53 +31,60 @@ int main(){
         for(int j=0;j<m;j++){
             cout<< setw(5) << a[i][j] <<" ";}
         cout<<"\n";}
+    a[h][v]=0;
     cout << "\n";
 
     // movimientos
-    for (int k=0; h<n-2; k++)
-        for (int w=0; v<m-2; w++){
-        int r=rand()%4;
-        cout << "Matriz movimiento "<<w+1<<":"<< endl;
-        switch (r) {
-            case 0: //NORTE
-                for (int i = 0; i < n; i++) {
-                    a[h+1][v] = 1;
-                    for (int j = 0; j < m; j++) {
-                        cout << setw(5) << a[i][j] << " ";}
-                    cout << "\n";}
-                h=h+1;
-                cout << "\n";
-                break;
+    for (int k=0; v<m-1; k++)
+        for (int w=1; h<n-1; w++){
+            int r=rand()%4;
+            cout << "Matriz movimiento "<<w<<":"<< endl;
+            switch (r) {
+                case 0: //NORTE
+                    for (int i = 0; i < n; i++) {
+                        a[h+1][v] = w;
+                        for (int j = 0; j < m; j++) {
+                            cout << setw(5) << a[i][j] << " ";}
+                        cout << "\n";}
+                    h=h+1;
+                    cout << "\n";
+                    break;
 
-            case 1: //SUR
-                for (int i = 0; i < n; i++) {
-                    a[h-1][v] = 1;
-                    for (int j = 0; j < m; j++) {
-                        cout << setw(5) << a[i][j] << " ";}
-                    cout << "\n";}
-                h=h-1;
-                cout << "\n";
-                break;
+                case 1: //SUR
+                    for (int i = 0; i < n; i++) {
+                        a[h-1][v] = w;
+                        for (int j = 0; j < m; j++) {
+                            cout << setw(5) << a[i][j] << " ";}
+                        cout << "\n";}
+                    h=h-1;
+                    cout << "\n";
+                    break;
 
-            case 2: //ESTE
-                for (int i = 0; i < n; i++) {
-                    a[h][v+1] = 1;
-                    for (int j = 0; j < m; j++) {
-                        cout << setw(5) << a[i][j] << " ";}
-                    cout << "\n";}
-                v=v+1;
-                cout << "\n";
-                break;
+                case 2: //ESTE
+                    for (int i = 0; i < n; i++) {
+                        a[h][v+1] = w;
+                        for (int j = 0; j < m; j++) {
+                            cout << setw(5) << a[i][j] << " ";}
+                        cout << "\n";}
+                    v=v+1;
+                    cout << "\n";
+                    break;
 
-            case 3: //OESTE
-                for (int i = 0; i < n; i++) {
-                    a[h][v-1] = 1;
-                    for (int j = 0; j < m; j++) {
-                        cout << setw(5) << a[i][j] << " ";}
-                    cout << "\n";}
-                v=v-1;
-                cout << "\n";
-                break;
-        }}
+                case 3: //OESTE
+                    for (int i = 0; i < n; i++) {
+                        a[h][v-1] = w;
+                        for (int j = 0; j < m; j++) {
+                            cout << setw(5) << a[i][j] << " ";}
+                        cout << "\n";}
+                    v=v-1;
+                    cout << "\n";
+                    break;
+            }}
 
     return 0;}
+
+//La matriz es siempre de dimensiones impar.
+//La posicion de partida es el centro de la matriz.
+//Las siguientes posiciones son aleatorias, ya sean norte, sur, este u oeste.
+//Termina la secuencia cuando el ultimo valor se encuentra en un borde de la matriz.
+
